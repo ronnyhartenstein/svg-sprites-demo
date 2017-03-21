a6b319d08e55a25b1a46feea4e0f294d0b7cc117
Ronny Hartenstein <r.hartenstein@axilaris.de>
2017-03-21 11:38:29 +0100

    Icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,17 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     svgstore: {
-		options: {
-			prefix : 'icon-' + (!!praefix ? praefix + '_' : '')
-		},
-		files: { 
-            'icons_svgsprite.svg': 'icons/*.svg'
-        }
-    },
+      icons: {
+        files: { 
+          'icons_svgsprite.svg': 'icons/*.svg'
+        }}},
     watch: {
-      files: ['icons/*.svg'],
-      tasks: ['svgstore']
-    }
+      svgstore: {
+        files: ['icons/*.svg'],
+        tasks: ['svgstore']
+        }}
   });
   grunt.loadNpmTasks('grunt-svgstore');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default', ['svgstore']);
 };
